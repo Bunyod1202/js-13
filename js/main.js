8e9ecfbd68faf137df692f29afdd24cc20b6c2ed
Bunyod <84246289+Bunyood@users.noreply.github.com>
2022-10-04 16:50:54 +0500

    bug fix

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 let elinputName = document.querySelector(".name")
 let elinputRship = document.querySelector(".relotionship")
 let elinputPhone = document.querySelector(".phone")
-let ellist = document.querySelector(".list")
 let elform = document.querySelector(".form")
 let eltoast = document.querySelector(".toasts")
 let eltoasta = document.querySelector(".toasta")
+let ellist = document.querySelector(".list")
 
 let array = JSON.parse(window.localStorage.getItem("lastname")) || [];
 
@@ -18,19 +18,21 @@ elform.addEventListener("submit", function (evt) {
     eltoasta.classList.remove("show")
   }, 4000);
   let obj = {
-    id: array.length + 1,
+    id: array.length > 0 ? array[array.length -1].id+1 :1,
     name: elinputNameValue,
     relotionship: elinputRshipValue,
     phone: elinputPhoneValue,
   }
+
   let arrays = array.findIndex(function(item){
       
     if (item.phone === elinputPhoneValue) {
     return 1202;
     } 
   })
+
   console.log(arrays);
-   if (arrays == 0) {
+   if (arrays > -1) {
     array.push()
     eltoast.classList.add("show")
     
@@ -43,53 +45,83 @@ elform.addEventListener("submit", function (evt) {
 
   adds(array)
   window.localStorage.setItem("lastname", JSON.stringify(array));
-
+  
   elinputName.value = ""
   elinputRship.value = ""
   elinputPhone.value = ""  
 })
 
-adds(array)
+
 
 function adds(arr) {
+  
   ellist.innerHTML = "";
 
   arr.forEach(function (item ) {
     let items = document.createElement("li")
-    let name = document.createElement("p")
+    let name = document.createElement("textarea")
     let number = document.createElement("span")
-    let Rships = document.createElement("p")
+    let Rships = document.createElement("textarea")
     let phone = document.createElement("a")
+    let dell = document.createElement("button")
     
+    items.dataset.id = item.id
+    dell.dataset.id = item.id
+
+    name.classList.add("textarea")
+    Rships.classList.add("textarea")
+
+
     items.classList.add("list-unstyled","text-center", "px-5", "py-3", "alert-success", "mt-3", "border", "border-success", "border-4", "border-start-0","w-100", "border-end-0", "rounded-pill","mx-auto")
     // tex.classList.add("ms-2","text-success")
     number.classList.add("text-success","mb-5","fw-bold", )
+    dell.classList.add("btn","btn-danger","d-block","m-auto","mt-3","w-50")
     
     
     number.textContent = item.id;
     name.textContent = item.name;
     Rships.textContent = item.relotionship;
     phone.textContent = item.phone;
+    dell.textContent = "del";
 
     phone.href = `tel:${item.phone}`
 
+    items.dataset.Id = item.id;
+    dell.dataset.Id = item.id;
+
     items.appendChild(number)
-    items.appendChild(name)
+    items.appendChild(name) 
     items.appendChild(Rships)
     items.appendChild(phone)
+    items.appendChild(dell)
     ellist.appendChild(items)
     
  
-
+  
 
   })
- 
+  
 };
 
+dellfunction(ellist)
+
+function dellfunction(ellist) {
+	ellist.addEventListener("click", (evt) => {
+	    
+	  if (evt.target.matches(".btn")) {
+	    let buttonId = Number(evt.target.dataset.Id);
+	    let items = array.findIndex(item => item.id == buttonId);
+      array.splice(items, 1);
+      window.localStorage.setItem("lastname", JSON.stringify(array));
+	    adds(array)
+	  
+	  }
+	   
+	});
+}
 
 
-
-
+adds(array);
 
 [].forEach.call(document.querySelectorAll('.phone'), function (input) {
   let keyCode;
@@ -123,6 +155,3 @@ function adds(arr) {
         input.addEventListener("blur", mask, false);
         input.addEventListener("keydown", mask, false);
       });
-      
-      
-    
